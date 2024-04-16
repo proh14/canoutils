dfa982ada13389be8d8d4c402a4b1145158da4c9
CobbCoding <rircobb@gmail.com>
2024-04-16 19:55:25 -0400

    add -d flag

--- src/rm/rm.c
+++ src/rm/rm.c
@@ -10,18 +10,12 @@
 #define VERSION "1.0.0"
 #define AUTHOR "CobbCoding"
 
-typedef enum {
-  VERBOSE,
-  FLAG_COUNT,
-} Flag;
-
-typedef struct {
-  Flag data[FLAG_COUNT];
-  size_t count;
-} Flags;
+bool verbose = false;
+bool prompt_every = false;
+bool remove_dir = false;
 
 char **shift(int *argc, char ***argv);
-int rm(char *filename, Flags flags);
+int rm(char *filename);
 
 #define print_version()                                                        \
   do {                                                                         \
@@ -34,6 +28,14 @@ int rm(char *filename, Flags flags);
     fprintf(stderr, "see rm --help\n");                                        \
     exit(1);                                                                   \
   } while (0)
+  
+#define shift_flags()  \
+  do {  \
+  if (argc == 0) {  \
+    print_not_enough();  \
+  }  \
+  flag = *shift(&argc, &argv);  \
+  } while(0)
 
 char **shift(int *argc, char ***argv) {
   char **result = *argv;
@@ -42,11 +44,13 @@ char **shift(int *argc, char ***argv) {
   return result;
 }
 
-int rm(char *filename, Flags flags) {
+int rm(char *filename) {
   assert(filename != NULL);
-  if (opendir(filename) != NULL) {
-    fprintf(stderr, "`%s` is a directory\n", filename);
-    exit(1);
+  if(!remove_dir) {
+    if (opendir(filename) != NULL) {
+      fprintf(stderr, "`%s` is a directory\n", filename);
+      exit(1);
+    }
   }
   int err = remove(filename);
   if (err == -1) {
@@ -54,16 +58,10 @@ int rm(char *filename, Flags flags) {
     exit(1);
   }
 
-  for (size_t i = 0; i < flags.count; i++) {
-    switch (flags.data[i]) {
-    case VERBOSE:
-      printf("removing `%s`\n", filename);
-      break;
-    default:
-      continue;
-    }
+  if(verbose) {
+    printf("removing `%s`\n", filename); 
   }
-
+    
   return err;
 }
 
@@ -71,19 +69,15 @@ int main(int argc, char **argv) {
   char *program = *shift(&argc, &argv);
   (void)program;
   char *filename = NULL;
+  char *flag = NULL;
   if (argc == 0) {
     fprintf(stderr, "not enough args\n");
     fprintf(stderr, "see rm --help\n");
     exit(1);
   }
-
-  if (argc == 0) {
-    print_not_enough();
-  }
-
-  Flags flags = {0};
-
-  char *flag = *shift(&argc, &argv);
+    
+  shift_flags();
+  
   if (strcmp(flag, "--version") == 0) {
     print_version();
     return 0;
@@ -91,17 +85,36 @@ int main(int argc, char **argv) {
     system("man rm");
     return 0;
   } else if (strcmp(flag, "-v") == 0) {
-    flags.data[flags.count++] = VERBOSE;
-    if (argc == 0) {
-      print_not_enough();
-    }
-    filename = *shift(&argc, &argv);
+    verbose = true;
+    shift_flags();
+    filename = flag;    
+  } else if(strcmp(flag, "-i") == 0) {
+    prompt_every = true;
+    shift_flags();    
+    filename = flag;    
+  } else if(strcmp(flag, "-d") == 0) {
+    remove_dir = true;
+    shift_flags();     
+    filename = flag;
   } else {
     filename = flag;
   }
 
   while (argc >= 0) {
-    rm(filename, flags);
+    if(prompt_every) {
+      printf("remove file `%s`? ", filename);
+      char prompt[16] = {0};
+      int err = scanf("%s", prompt);
+      if(err == EOF) {
+        fprintf(stderr, "error: EOF\n");
+        exit(1);
+      }
+      if(strncmp(prompt, "y", sizeof("y")-1) != 0) {
+        filename = *shift(&argc, &argv);
+        continue;
+      }
+    }
+    rm(filename);
     filename = *shift(&argc, &argv);
   }
   return 0;
