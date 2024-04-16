f8317dc3b52e2d557657ed95f5172cd6e84f9e84
CobbCoding <rircobb@gmail.com>
2024-04-16 18:58:30 -0400

    implement --version flag

--- src/rm/rm.c
+++ src/rm/rm.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-  printf("Hello, World!\n");
+#define NAME "rm (canoutils)"
+#define VERSION "1.0.0"
+#define AUTHOR "CobbCoding"
+
+char **shift(int *argc, char ***argv);
+
+#define print_version()                                                        \
+  do {                                                                         \
+    printf("%s\nversion: %s\nby: %s\n", NAME, VERSION, AUTHOR);               \
+  } while (0)
+  
+char **shift(int *argc, char ***argv) {
+  char **result = *argv;
+  *argv += 1;
+  *argc -= 1;
+  return result;
+}
+
+int main(int argc, char **argv) {
+  char *program = *shift(&argc, &argv);
+  (void)program;
+  char *filename = NULL;
+  if(argc == 0) exit(1);
+  char *flag = *shift(&argc, &argv);
+  if(strcmp(flag, "--version") == 0) {
+    print_version();
+  } else {
+    filename = flag;
+    printf("%s\n", filename);
+  }
   return 0;
 }
