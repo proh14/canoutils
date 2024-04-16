859b99a85570756f2b82afa333a07a7229637cc7
Sigmanificient <edhyjox@gmail.com>
2024-04-16 04:11:09 +0200

    Setup the Makefile factory

---
+++ base.mk
@@ -0,0 +1,45 @@
+.POSIX:
+.SUFFIXES:
+
+# ↓ `touch .fast` to force multi-threading
+ifneq ($(shell find . -name ".fast"),)
+    MAKEFLAGS += -j
+endif
+
+V ?= 0
+ifneq ($(V),0)
+  Q :=
+else
+  Q := @
+endif
+
+RM ?= rm -f
+AR ?= ar
+
+ifneq ($(shell command -v tput),)
+  ifneq ($(shell tput colors),0)
+
+C_RESET := \033[00m
+C_BOLD := \e[1m
+C_RED := \e[31m
+C_GREEN := \e[32m
+C_YELLOW := \e[33m
+C_BLUE := \e[34m
+C_PURPLE := \e[35m
+C_CYAN := \e[36m
+
+  endif
+endif
+
+NOW = $(shell date +%s%3N)
+STIME := $(call NOW)
+TIME_NS = $(shell expr $(call NOW) - $(STIME))
+TIME_MS = $(shell expr $(call TIME_NS))
+
+BOXIFY = "[$(C_BLUE)$(1)$(C_RESET)] $(2)"
+
+ifneq ($(shell command -v printf),)
+  LOG_TIME = printf $(call BOXIFY, %6s , %b\n) "$(call TIME_MS)"
+else
+  LOG_TIME = echo -e $(call BOXIFY, $(call TIME_MS) ,)
+endif
---
+++ commons.mk
@@ -0,0 +1,57 @@
+CC = gcc
+
+CFLAGS := -std=c99 -pedantic
+CFLAGS += -MMD -MP
+CFLAGS += -pipe
+
+CFLAGS += -O2 -march=native -mtune=native
+CFLAGS += -ffunction-sections -fdata-sections
+CFLAGS += -Wp,-U_FORTIFY_SOURCE
+
+CFLAGS += -Wall -Wextra
+CFLAGS += -Wcast-qual
+CFLAGS += -Wconversion
+CFLAGS += -Werror=return-type
+CFLAGS += -Werror=vla-larger-than=0
+CFLAGS += -Wmissing-prototypes
+CFLAGS += -Wshadow
+CFLAGS += -Wstrict-prototypes
+CFLAGS += -Wwrite-strings
+
+CFLAGS += -iquote src
+
+LDFLAGS := -fwhole-program -flto
+LDFLAGS += -Wl,--gc-sections
+BUILD_DIR := .build
+
+LDFLAGS := -fwhole-program -flto
+
+OBJ := $(SRC:%.c=$(BUILD_DIR)/%.o)
+
+.PHONY: all
+all: $(OUT)
+
+-include $(DEP)
+
+$(OUT): $(OBJ)
+	@ mkdir -p $(dir $@)
+	$Q $(CC) -o $@ $(OBJ) $(CFLAGS) $(LDLIBS) $(LDFLAGS)
+	@ $(LOG_TIME) "LD $(C_GREEN) $@ $(C_RESET)"
+
+$(BUILD_DIR)/%.o: %.c
+	@ mkdir -p $(dir $@)
+	$Q $(CC) $(CFLAGS) -o $@ -c $< || exit 1
+	@ $(LOG_TIME) "CC $(C_PURPLE) $(notdir $@) $(C_RESET)"
+
+.PHONY: clean
+clean:
+	$(RM) $(OBJ)
+
+.PHONY: fclean
+fclean: clean
+	$(RM) -r $(BUILD_DIR) $(OUT) $(OUT_DEBUG)
+
+.PHONY: re
+.NOTPARALLEL: re
+re: fclean
+	@ $(MAKE) all
