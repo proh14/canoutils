e03d477cf54a0c1fe3c56200d23be3daa3aea400
Eike Flath <eike9008@gmail.com>
2024-04-20 14:16:17 +0200

    made sleep handle SIGALRM correctly

--- src/sleep/sleep.c
+++ src/sleep/sleep.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define NAME "sleep (canoutils)"
 #define VERSION "1.0.0"
@@ -27,6 +28,11 @@ static unsigned int parse_int(char *arg) {
   return n;
 }
 
+static void handle_sigalrm(int sig) {
+  (void) sig;
+  exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "sleep: expected exactly one operand, got %d\n", argc - 1);
@@ -36,6 +42,7 @@ int main(int argc, char **argv) {
     print_version();
     return EXIT_SUCCESS;
   }
-  sleep(parse_int(argv[1]));
-  return EXIT_SUCCESS;
+  unsigned int n = parse_int(argv[1]);
+  signal(SIGALRM, handle_sigalrm);
+  return sleep(n) == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
