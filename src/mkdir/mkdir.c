dd5da3c1a296301724951abdc380e27904f4ed9a
Hoorad Farrokh <hourrad.f@gmail.com>
2024-04-21 15:24:56 +1200

    Add one to optind at invalid option so filename is not in argv list

--- src/mkdir/mkdir.c
+++ src/mkdir/mkdir.c
@@ -1,6 +1,16 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <unistd.h>
 
-int main(void) {
-  printf("Hello, World!\n");
+int main(int argc, char **argv) {
+  int ch;
+  while ((ch = getopt(argc, argv, "a:b")) != -1) {
+    printf("ch = %c\n", ch);
+  }
+  argv += optind;
+  argc -= optind;
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
   return 0;
 }
