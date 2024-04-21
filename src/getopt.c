0498ab4ac93fd032113921dd027def27124def1f
CobbCoding <rircobb@gmail.com>
2024-04-20 20:07:40 -0400

    clang format

--- src/getopt.c
+++ src/getopt.c
@@ -6,15 +6,16 @@
 char *optarg = NULL;
 int optind = 1, opterr = 0, optopt = '\0';
 
-#define EXCHANGE(coropt) do { \
-  int i = (coropt);  \
-  int j = (coropt) - 1;  \
-  while (j >= 0 && argv[j][0] != '-') {  \
-    getopt_exchange(argv, i, j);  \
-    i--;  \
-    j--;  \
-  }  \
-} while(0)
+#define EXCHANGE(coropt)                                                       \
+  do {                                                                         \
+    int i = (coropt);                                                          \
+    int j = (coropt)-1;                                                        \
+    while (j >= 0 && argv[j][0] != '-') {                                      \
+      getopt_exchange(argv, i, j);                                             \
+      i--;                                                                     \
+      j--;                                                                     \
+    }                                                                          \
+  } while (0)
 
 static void getopt_printerr(const char *msg) {
   if (opterr) {
@@ -106,7 +107,8 @@ exit: {
   return c;
 }
 
-int getopt_long(int argc, char *argv[], const char *optstring, const struct option *longopts, int *longindex) {
+int getopt_long(int argc, char *argv[], const char *optstring,
+                const struct option *longopts, int *longindex) {
   (void)longindex;
   int c;
   static char *nextchar = NULL;
@@ -129,34 +131,34 @@ int getopt_long(int argc, char *argv[], const char *optstring, const struct opti
     }
     nextchar = &argv[coropt][1];
   }
-		
-  if(*nextchar == '-') {
-	nextchar++;
-	while(longopts->name != NULL) {
-		if(strcmp(longopts->name, nextchar) == 0) {
-            optind++;	
-            nextchar = NULL;                    
-			c = longopts->val;
-            if(longopts->has_arg) {
-              EXCHANGE(coropt);
-              coropt++;
-              if (coropt >= argc || argv[coropt][0] == '-') {
-                getopt_printerr("option requires an argument\n");
-                optopt = longopts->val;
-                c = '?';
-                goto exit;
-              }
-              optarg = argv[coropt];
-              optind++;
-            }
-			goto exit;
-		}			
-		longopts++;
-	}
-	optind++;
-    optopt = longopts->val;    
-	nextchar = NULL;
-	getopt_printerr("invalid option\n");
+
+  if (*nextchar == '-') {
+    nextchar++;
+    while (longopts->name != NULL) {
+      if (strcmp(longopts->name, nextchar) == 0) {
+        optind++;
+        nextchar = NULL;
+        c = longopts->val;
+        if (longopts->has_arg) {
+          EXCHANGE(coropt);
+          coropt++;
+          if (coropt >= argc || argv[coropt][0] == '-') {
+            getopt_printerr("option requires an argument\n");
+            optopt = longopts->val;
+            c = '?';
+            goto exit;
+          }
+          optarg = argv[coropt];
+          optind++;
+        }
+        goto exit;
+      }
+      longopts++;
+    }
+    optind++;
+    optopt = longopts->val;
+    nextchar = NULL;
+    getopt_printerr("invalid option\n");
     c = '?';
     goto exit;
   }
@@ -165,10 +167,10 @@ int getopt_long(int argc, char *argv[], const char *optstring, const struct opti
   if (!((idx = getopt_in(*nextchar, optstring)) >= 0)) {
     getopt_printerr("invalid option\n");
     optopt = *nextchar;
-	if(*(nextchar+1) == '\0') {
-		nextchar = NULL;
-		optind++;
-	}
+    if (*(nextchar + 1) == '\0') {
+      nextchar = NULL;
+      optind++;
+    }
     c = '?';
     goto exit;
   }
@@ -177,14 +179,14 @@ int getopt_long(int argc, char *argv[], const char *optstring, const struct opti
   if (*nextchar == '\0') {
     coropt++;
     nextchar = NULL;
-	optind++;
+    optind++;
   }
 
   if (optstring[idx + 1] != ':') {
     coropt--;
     goto exit;
   }
-  EXCHANGE(coropt-1);      
+  EXCHANGE(coropt - 1);
 
   if (nextchar != NULL && *nextchar != '\0') {
     coropt++;
@@ -196,12 +198,10 @@ int getopt_long(int argc, char *argv[], const char *optstring, const struct opti
     c = '?';
     goto exit;
   }
-  
+
   optarg = argv[coropt];
   optind++;
 
-exit: {
-  EXCHANGE(coropt);
-}
+exit: { EXCHANGE(coropt); }
   return c;
 }
