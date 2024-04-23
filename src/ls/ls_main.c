3bb50ffb5b72a0ca00846972e0ed90324a23b349
Sigmanificient <edhyjox@gmail.com>
2024-04-24 01:40:11 +0200

    Migrate ls command to getopt

--- src/ls/ls_main.c
+++ src/ls/ls_main.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,21 +9,65 @@
 #define VERSION "1.0.0"
 #define AUTHOR "Yohann Boniface (Sigmanificient)"
 
+#include "cgetopt.h"
 #include "version_info.h"
 
-static const char FLAGLIST[] = "alRdrt";
 static char DEFAULT_LOCATION[] = ".";
 
-static char compose_flaglist(int argc, char **argv) {
-  int flags = 0;
+enum {
+  GETOPT_VERSION_CHAR = (CHAR_MIN - 1),
+  GETOPT_SORT_CHAR = (CHAR_MIN - 2),
+};
 
-  for (int i = 1; i < argc; i++) {
-    if (argv[i][0] != '-' || argv[i][1] == '\0')
-      continue;
-    for (int j = 1; argv[i][j] != '\0'; j++)
-      flags |= 1 << (stridx(FLAGLIST, argv[i][j]) + 1);
+static const struct option LONG_OPTIONS[] = {
+    {"version", no_argument, NULL, GETOPT_VERSION_CHAR},
+    {"all", no_argument, NULL, 'a'},
+    {"recursive", no_argument, NULL, 'R'},
+    {"directory", no_argument, NULL, 'd'},
+    {"reverse", no_argument, NULL, 'r'},
+    {"sort", required_argument, NULL, GETOPT_SORT_CHAR},
+    {0}};
+
+static char compose_flaglist(int argc, char **argv) {
+  char flags = 0;
+  int oi = -1;
+
+  for (;;) {
+    int c = getopt_long(argc, argv, "alRdrt", LONG_OPTIONS, &oi);
+
+    if (c == -1)
+      return flags;
+    switch (c) {
+    case 'a':
+      flags |= F_ALL_FILES;
+      break;
+    case 'l':
+      flags |= F_LONG_FORM;
+      break;
+    case 'R':
+      flags |= F_RECURSIVE;
+      break;
+    case 'd':
+      flags |= F_DIRECTORY;
+      break;
+    case 'r':
+      flags |= F_REV_ORDER;
+      break;
+    case 't':
+      flags |= F_SORT_TIME;
+      break;
+    case (CHAR_MAX + 1):
+      if (!strcmp(optarg, "time"))
+        flags |= F_SORT_TIME;
+      break;
+    case GETOPT_VERSION_CHAR:
+      print_version();
+      return -1;
+    default:
+      fprintf(stderr, "ls: invalid option -- '%c'\n", c);
+      return -2;
+    }
   }
-  return (char)(flags >> 1);
 }
 
 static size_t count_targets(int argc, char **argv) {
@@ -55,15 +100,11 @@ static bool list_dirs(dirbuff_t *db, int argc, char **argv, char flags) {
 
 int main(int argc, char **argv) {
   dirbuff_t db = {.size = MIN_ALLOCATED_ENTRY};
-  char flags;
+  char flags = compose_flaglist(argc, argv);
   int err = 0;
 
-  for (int i = 0; argv[i] != NULL; i++)
-    if (!strcmp(argv[i], "--version")) {
-      print_version();
-      return EXIT_SUCCESS;
-    }
-  flags = compose_flaglist(argc, argv);
+  if (flags < 0)
+    return (flags == -1) ? EXIT_SUCCESS : EXIT_FAILURE;
   db.entries = malloc(db.size * sizeof(*db.entries));
   if (db.entries == NULL)
     return EXIT_FAILURE;
